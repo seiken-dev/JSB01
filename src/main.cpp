05a2ee0962e7dc12abc63d7820cf6fe8f1a437fa
KIRIAKE Masanori <seiken@rd01.net>
2023-07-30 01:21:52 +0900

    * MB10XXの反応を確認するためのテスト。   できるだけ計測してからの遅延が発生しないように配慮したつもり。

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "SerialUSB.h"
 #include <Arduino.h>
 #include <cstdint>
 constexpr uint8_t pin_sonar = D5;
@@ -6,6 +7,9 @@ constexpr uint8_t pin_button2 = D1;
 constexpr uint8_t pin_vibe = D2;
 
 constexpr uint8_t flash_ms = 10;
+
+static uint16_t detectRange=2000;
+bool changedDetectRange = false;
 
 void flash(uint16_t ms = flash_ms) {
   digitalWrite(pin_vibe, HIGH);
@@ -21,18 +25,41 @@ void setup() {
   pinMode(pin_button1, INPUT_PULLUP);
   pinMode(pin_button2, INPUT_PULLUP);
   pinMode(pin_vibe, OUTPUT);
-  delay(400); // wait USB Serial
-  flash();;
-  delay(100);
-  flash();
 }
 
 uint16_t ranging()
 {
   uint32_t time = pulseIn(pin_sonar, HIGH, 200*1000);
-  return (time > 10*147) ? uint16_t(time/147) : 0;
+  return (time > 300) ? time : 0;
 }
+
 void loop() {
   uint16_t distance = ranging();
-  if (distance < 80) flash();
+  if (distance < detectRange) {
+    if (!changedDetectRange) flash();
+  }
+}
+
+void setup1() { return; }
+
+bool isPressed(uint8_t button) {
+  return (digitalReadFast(button)) ? false : true;
+}
+
+void loop1() {
+  if (isPressed(pin_button1)) {
+    if (detectRange < 4500) detectRange += 500;
+    changedDetectRange = true;
+  }
+  else if (isPressed(pin_button2)) {
+    if (detectRange > 0) detectRange -= 500;
+    changedDetectRange = true;
+  }
+  if(changedDetectRange) {
+    for (uint c=0; c < detectRange/500; c++) {
+      flash();
+      delay(200);
+    }
+    changedDetectRange = false;
+  }
 }

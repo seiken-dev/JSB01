702fd855a5d4b0229726431747547fd3e05730a0
KIRIAKE Masanori <seiken@rd01.net>
2023-08-02 02:05:09 +0900

    * 美しくないけど、ログが取れる。 * 下ボタンを数回押して、検出スレッショルドを０にすると、ログの記録が止まる。

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include "Wire.h"
-#include "SparkFun_VL53L1X.h" //Click here to get the library: http://librarymanager/All#SparkFun_VL53L1X
+#include "VL53L1X.h"
 #ifdef ARDUINO_SEEED_XIAO_RP2040
 #include <SingleFileDrive.h>
 #include "LittleFS.h"
 #endif
 
+VL53L1X Tof;
 constexpr uint8_t pin_sonar = D5;
 constexpr uint8_t pin_button1 = D0;
 constexpr uint8_t pin_button2 = D1;
@@ -16,7 +17,9 @@ constexpr uint8_t flash_ms = 10;
 uint16_t detectRange=2000;
 bool changedDetectRange = false;
 
-SFEVL53L1X distanceSensor;
+File logFile;
+bool isLogging = false;
+
 
 void flash(uint16_t ms = flash_ms) {
   digitalWrite(pin_vibe, HIGH);
@@ -56,15 +59,16 @@ MB_Type detectMBType() {
   return type;
 }
 
-
 bool detectVL() {
   Serial.println("Detecting VL53L1X");
-
-  if (distanceSensor.begin() != 0) //Begin returns 0 on a good init
+  if (!Tof.init())
   {
     Serial.println("Sensor failed to begin. Please check wiring. Freezing...");
     return false;
   }
+  Tof.setDistanceMode(VL53L1X::Long);
+  Tof.setMeasurementTimingBudget(50000);
+  Tof.startContinuous(50);
   return true;
 }
 
@@ -81,14 +85,17 @@ uint16_t mbRanging(MB_Type type)
 }
 
 unsigned int vlRanging() {
-  distanceSensor.startRanging(); //Write configuration bytes to initiate measurement
-  while (!distanceSensor.checkForDataReady())
-  {
-    delay(1);
+  Tof.read();
+  uint16_t distance = Tof.ranging_data.range_mm;
+  auto status = Tof.ranging_data.range_status;
+  float ambient = Tof.ranging_data.ambient_count_rate_MCPS;
+  float peak = Tof.ranging_data.peak_signal_count_rate_MCPS;
+  if (isLogging) {
+    logFile.printf("%lu,%u,%d,%f,%f\n", millis(), distance, status, ambient, peak);
   }
-  int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
-  distanceSensor.clearInterrupt();
-  distanceSensor.stopRanging();
+  if (status == 0) {
+    distance = Tof.ranging_data.range_mm;
+  }
   return distance;
 }
 
@@ -105,6 +112,7 @@ unsigned int ranging() {
 bool isPressed(uint8_t button) {
   return (digitalRead(button)) ? false : true;
 }
+
 void loop1()
 {
   if (isPressed(pin_button1)) {
@@ -112,7 +120,15 @@ void loop1()
     changedDetectRange = true;
   }
   else if (isPressed(pin_button2)) {
-    if (detectRange > 0) detectRange -= 500;
+    if (detectRange > 0) {
+      detectRange -= 500;
+      if (detectRange == 0)  {
+        isLogging=false;
+        logFile.close();
+        Serial.println("Closing logfile.");
+      }
+    }
+    
     changedDetectRange = true;
   }
   if(changedDetectRange) {
@@ -135,9 +151,48 @@ void UserCommandTask(void *pvParameters)
 
 #endif
 
+#ifdef ARDUINO_SEEED_XIAO_RP2040
+// Called when the USB stick connected to a PC and the drive opened
+// Note this is from a USB IRQ so no printing to SerialUSB/etc.
+void plug(uint32_t i) {
+  (void) i;
+  isLogging = false;
+  logFile.close();
+}
+
+// Called when the USB is ejected or removed from a PC
+// Note this is from a USB IRQ so no printing to SerialUSB/etc.
+void unplug(uint32_t i) {
+  (void) i;
+  isLogging = true;
+}
+
+// Called when the PC tries to delete the single file
+// Note this is from a USB IRQ so no printing to SerialUSB/etc.
+void deleteCSV(uint32_t i) {
+  (void) i;
+  LittleFS.remove("vl53l1x.csv");
+}
+#endif
+
 void setup() {
+#ifdef ARDUINO_SEEED_XIAO_RP2040
+  if (!LittleFS.begin()) {
+    Serial.println("Filesystem initialization error.");
+    return;
+  }
+  String logFileName = "vl53l1x.csv";
+  logFile = LittleFS.open(logFileName.c_str(), "a");
+  logFile.printf("time,distance,status,ambient,peak\n");
+  isLogging=true;
+  delay(100);
+  singleFileDrive.onDelete(deleteCSV);
+  singleFileDrive.onPlug(plug);
+  singleFileDrive.onUnplug(unplug);
+  singleFileDrive.begin(logFileName.c_str(), logFileName.c_str());
+#endif
   Serial.begin(115200);
-  delay(1000);
+  delay(3000);
   Serial.println("start");
   pinMode(pin_button1, INPUT_PULLUP);
   pinMode(pin_button2, INPUT_PULLUP);

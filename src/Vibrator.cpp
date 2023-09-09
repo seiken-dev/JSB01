5faea158d946b93b7e02491f62c3e7e955804ef5
KIRIAKE Masanori <seiken@rd01.net>
2023-09-10 05:51:06 +0900

    * RP2040ではtone()を使うように変更

--- src/Vibrator.cpp
+++ src/Vibrator.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <cstdint>
 #include "Vibrator.h"
 
 #ifdef ARDUINO_SEEED_XIAO_RP2040
@@ -7,78 +6,45 @@
 #endif
 
 constexpr uint8_t pwmCH = 0;
-constexpr uint32_t pwmFreq = 126;
+constexpr uint32_t pwmFreq = 130;
 constexpr uint8_t pwmResolution = 13;
 #ifdef ARDUINO_XIAO_ESP32C3
-hw_timer_t *vibeClock = nullptr;
+hw_timer_t *vibClock = nullptr;
 #endif
 bool Vibrator::begin(uint8_t pin, bool init, bool ledc)
 {
-  if (init && !ledc) {
+  if (init) {
     pinMode(pin, OUTPUT);
   }
   _pin = pin;
   if (init && ledc) {
-#ifdef ARDUINO_SEEED_XIAO_RP2040
-    gpio_set_function(_pin, GPIO_FUNC_PWM);
-    _pwmSlice = pwm_gpio_to_slice_num(_pin);
-    pwm_set_clkdiv(_pwmSlice,
-		   (float(F_CPU) / (pwmFreq * (1 << pwmResolution)))
-		   );
-    pwm_set_wrap(_pwmSlice, (1 << pwmResolution)-1);
-    pwm_set_chan_level(_pwmSlice, PWM_CHAN_A, 1 << (pwmResolution-1));
-#else
-    vibeClock = timerBegin(0, 80, true);
-    timerAttachInterrupt(vibeClock, vibeOff, true);
+#ifdef ARDUINO_XIAO_ESP32C3
+    vibClock = timerBegin(0, 80, true);
+    timerAttachInterrupt(vibClock, off, true);
     ledcSetup(pwmCH, pwmFreq, pwmResolution);
     ledcAttachPin(_pin, pwmCH);
 #endif
+    _freq = pwmFreq;
   }
   _ledc = ledc;
   _on = false;
   return true;
 }
 
-#ifdef ARDUINO_SEEED_XIAO_RP2040
-void Vibrator::on() {
-  if (_ledc) {
-    pwm_set_enabled(_pwmSlice, true);
-  } else {
-    digitalWrite(_pin, HIGH);
-  }
-  _on = true;
-}
-
-void Vibrator::on(uint16_t ms)
-{
-  on();
-  add_alarm_in_ms(ms, vibOffCB, nullptr, false);
-}
-
-void Vibrator::off() {
-  if (_ledc) {
-    pwm_set_enabled(_pwmSlice, false);
-  } else {
-    digitalWrite(_pin, LOW);
-  }
-  _on = false;
-}
-void Vibrator::setFrequency(uint32_t f)
-{
-  if (_ledc) {
-    pwm_set_enabled(_pwmSlice, false);
-    pwm_set_clkdiv(_pwmSlice,
-		   (float(F_CPU) / (f * (1 << pwmResolution)))
-		   );
-  }
-}
-#else
+#ifdef ARDUINO_XIAO_ESP32C3
 void IRAM_ATTR Vibrator::on() {
   if (_ledc) {
     ledcWrite(pwmCH, 1 << (pwmResolution - 1));
   } else {
     digitalWrite(_pin, HIGH);
   }
+}
+
+void IRAM_ATTR Vibrator::on(uint16_t ms) {
+  timerWrite(vibClock, 0);
+  on();
+  timerAlarmWrite(vibClock, ms*1000, false);
+  timerAlarmEnable(vibClock);
 }
 
 void IRAM_ATTR Vibrator::off() {
@@ -89,20 +55,54 @@ void IRAM_ATTR Vibrator::off() {
   }
 }
 
-void Vibrator::setFrequency(uint32_t f)
+void Vibrator::setFrequency(unsigned int f)
 {
   if (_ledc) ledcChangeFrequency(pwmCH, f, pwmResolution);
+  _freq = f;
 }
-#endif
+#else
+void Vibrator::on() {
+  if (_ledc) {
+    tone(_pin, _freq);
+  } else {
+    digitalWrite(_pin, HIGH);
+  }
+  _on = true;
+}
+void Vibrator::on(uint16_t ms)
+{
+  if (_ledc) {
+    tone(_pin, _freq, ms);
+  } else {
+    on();
+    add_alarm_in_ms(ms, vibOffCB, nullptr, false);
+  }
+}
+
+void Vibrator::off() {
+  if (_ledc) {
+    noTone(_pin);
+  } else {
+    digitalWrite(_pin, LOW);
+  }
+  _on = false;
+}
+void Vibrator::setFrequency(unsigned int f)
+{
+  if (_ledc) {
+    _freq = f;
+  }
+}
 
 int64_t Vibrator::vibOffCB(alarm_id_t id, void *user_data)
 {
   off();
   return 0;
 }
+#endif
 
 uint8_t Vibrator::_pin;
 bool Vibrator::_ledc;
-uint8_t Vibrator::_pwmSlice;
 bool Vibrator::_on;
+unsigned int Vibrator::_freq;
 Vibrator vib;
--- src/Vibrator.h
+++ src/Vibrator.h
@@ -10,13 +10,13 @@ class Vibrator {
   static void on(uint32_t f, uint16_t ms);
   static void on(uint16_t ms);
   static void off();
-  static void setFrequency(uint32_t f);
+  static void setFrequency(unsigned int f);
   static bool isOn() { return _on; };
  private:
   static uint8_t _pin;
   static bool _ledc;
-  static uint8_t _pwmSlice;
   static bool _on;
+  static unsigned int _freq;
 #ifdef ARDUINO_SEEED_XIAO_RP2040
   static int64_t vibOffCB(alarm_id_t id, void *user_data);
 #endif

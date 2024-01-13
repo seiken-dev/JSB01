06162f0693643c496467353676c2ce3f29fcbc9d
KIRIAKE Masanori <seiken@rd01.net>
2024-01-13 11:51:18 +0900

    * Testing...

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include "pico/stdio.h"
 #include <iostream>
+#include <pico/stdlib.h>
+#include <hardware/gpio.h>
+#include "hardware/pwm.h"
 
+void gpioInit(uint n, bool dir) {
+  gpio_init(n);
+  gpio_set_dir(n, dir);
+  if (dir == GPIO_IN) gpio_pull_up(n);
+}
+constexpr uint sw = 2;
 int main(){
-  std::cout << "Hello world" << std::endl;
+  gpioInit(sw, GPIO_IN);
+  while(1) {
+    if (gpio_get(sw) == false) {
+      // Pushed
+    }
+    sleep_ms(10);
+  }
   return 0;
 }
